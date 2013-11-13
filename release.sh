a9abdd31f81d4a99b01c6002c78b2f142e1099a1
tamer <tamer@gametube.org>
2013-11-13 16:36:05 +0100

    added release script

---
+++ release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf target/release
+mkdir target/release
+cd target/release
+git clone git@github.com:tamershahin/redis-flexible-cache.git
+cd redis-flexible-cache
+grails clean
+grails compile
+grails publish-plugin --noScm
