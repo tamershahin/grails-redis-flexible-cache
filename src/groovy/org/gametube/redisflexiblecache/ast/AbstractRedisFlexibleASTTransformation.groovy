fbfdc2bbc65e9c2dcc7acb2ad517df6426208d15
tamer <tamer@gametube.org>
2013-11-13 18:50:07 +0100

    refactoring packages

--- src/groovy/org/gametube/redisflexiblecache/ast/AbstractRedisFlexibleASTTransformation.groovy
+++ src/groovy/org/gametube/redisflexiblecache/ast/AbstractRedisFlexibleASTTransformation.groovy
@@ -1,4 +1,20 @@
-package org.gametube.cache.redis.ast
+/*
+ * Copyright 2006-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gametube.redisflexiblecache.ast
 
 import org.codehaus.groovy.ast.*
 import org.codehaus.groovy.ast.builder.AstBuilder
@@ -13,7 +29,7 @@ import org.codehaus.groovy.control.messages.SyntaxErrorMessage
 import org.codehaus.groovy.syntax.SyntaxException
 import org.codehaus.groovy.transform.ASTTransformation
 import org.codehaus.groovy.transform.GroovyASTTransformation
-import org.gametube.dmg.core.cache.RedisFlexibleCachingService
+import org.gametube.redisflexiblecache.RedisFlexibleCachingService
 
 import static org.springframework.asm.Opcodes.ACC_PRIVATE
 import static org.springframework.asm.Opcodes.ACC_PUBLIC
