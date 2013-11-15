d2ea818737d31dc283e9738646dbfc626201cbfd
tamer <tamer@gametube.org>
2013-11-15 12:11:55 +0100

    fixed the re-injection of wrong redisService bean

--- RedisFlexibleCacheGrailsPlugin.groovy
+++ RedisFlexibleCacheGrailsPlugin.groovy
@@ -24,7 +24,7 @@ import org.springframework.core.serializer.support.SerializingConverter
 
 class RedisFlexibleCacheGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "0.1-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging
@@ -34,11 +34,12 @@ class RedisFlexibleCacheGrailsPlugin {
     def title = "Redis Flexible Cache Plugin" // Headline display name of the plugin
     def author = "Tamer Shahin"
     def authorEmail = "tamer@gametube.org"
-    def description = '''\
-This plugin is an alternative to redis-cache. It give the possibility to set the expire time in seconds for every cached keys.
-Using the redis plugin this feature is available using the @Memoize annotation given, but the it lacks the option to serialize
-any kind of Serializable object. This plugin is inspired by both but is not based on them.
-'''
+    def description = '''
+This plugin is an alternative to redis-cache-plugin. It gives the possibility to set the expire time in seconds for every cached key, and provides a service, annotations and injected methods to perform entry caching and eviction.
+The redis-plugin plugin also provides the possibility to set a TTL for a cached entry (using the provided @Memoize annotation), but the it lacks the option to serialize any kind of Serializable objects (only object ids are cached and then hydrated from main DB).
+This means that potentially a lot of data will go to redis, so pay attention to memory and bandwith consumption!
+This plugin is not an extension of cache-plugin plugin, it is far more simple and lighter at the same time. The cache-plugin gives a deep integration with grails Controller CoC mechanism, but i think it creates too much overhead sometimes.
+The cache implementation provided by this plugin is inspired by redis-cache-plugin and redis-plugin but is not based on them. This plugins depends on redis-plugin for communication with redis and therefore it uses its configuration DSL.'''
 
     // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/dmg-cache"
@@ -56,10 +57,10 @@ any kind of Serializable object. This plugin is inspired by both but is not base
     def developers = [[name: "Germán Sancho", email: "german@gametube.org"]]
 
     // Location of the plugin's issue tracker.
-    def issueManagement = [system: "GITHUB", url: "https://github.com/tamershahin/redis-flexible-cache/issues"]
+    def issueManagement = [system: "GITHUB", url: "https://github.com/tamershahin/grails-redis-flexible-cache/issues"]
 
     // Online location of the plugin's browseable source code.
-    def scm = [url: "https://github.com/tamershahin/redis-flexible-cache"]
+    def scm = [url: "https://github.com/tamershahin/grails-redis-flexible-cache"]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
@@ -86,6 +87,7 @@ any kind of Serializable object. This plugin is inspired by both but is not base
             redisService = ref('redisService' + connectionToUse)
             grailsApplication = ref('grailsApplication')
         }
+
     }
 
     def doWithDynamicMethods = { ctx ->
@@ -138,22 +140,23 @@ any kind of Serializable object. This plugin is inspired by both but is not base
 
         def redisFlexibleCS = mainContext.redisFlexibleCacheService
 
-        def clazzes = []
-        clazzes += mainContext.grailsApplication.controllerClasses*.clazz
-        clazzes += mainContext.grailsApplication.serviceClasses*.clazz
-        clazzes.each { cls ->
-            cls.metaClass.cache = { Map args, Closure closure ->
-                redisFlexibleCS.doCache(args.key, args.group, args.ttl, args.reAttachToSession ?: false, closure)
-            }
-            cls.metaClass.evictCache = { Map args, Closure closure = null ->
-                redisFlexibleCS.evictCache(args.key, closure)
-            }
-        }
-
         def redisCacheConfigMap = mergeConfigMaps(mainContext.grailsApplication)
         redisFlexibleCS.expireMap = redisCacheConfigMap?.expireMap ?: [:]
         redisFlexibleCS.defaultTTL = redisCacheConfigMap?.defaultTTL ?: 0
         redisFlexibleCS.enabled = redisCacheConfigMap?.enabled == false ?: true
+        redisFlexibleCS.redisService = mainContext."redisService${redisCacheConfigMap.connectionToUse.capitalize()}"
+
+        def clazzes = []
+        clazzes += mainContext.grailsApplication.controllerClasses*.clazz
+        clazzes += mainContext.grailsApplication.serviceClasses*.clazz
+        clazzes.each { cls ->
+            cls.metaClass.redisFlexibleCache = { Map args, Closure closure ->
+                redisFlexibleCS.doCache(args.key, args.group, args.ttl, args.reAttachToSession ?: false, closure)
+            }
+            cls.metaClass.evictRedisFlexibleCache = { Map args, Closure closure = null ->
+                redisFlexibleCS.evictCache(args.key, closure)
+            }
+        }
     }
 
 }
